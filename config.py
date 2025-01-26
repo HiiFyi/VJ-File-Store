ffb4ea4b1dee72b51008b112fa7931a174937f8c
HiiFyi <167199917+HiiFyi@users.noreply.github.com>
2025-01-26 21:32:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,11 +20,11 @@ def is_enabled(value, default):
 # Bot Information
 API_ID = int(environ.get("API_ID", "24935727"))
 API_HASH = environ.get("API_HASH", "3fd33336629324ecd664e9b6894f0909")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7629231918:AAFnGYdeQH27t0Pt7fS7PGV1pJJGYt4fo84")
 
 PICS = (environ.get('PICS', 'https://envs.sh/s/5d-PIVcAhv2lf8ltfMQ2Ig/ACR.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "") # without @
+BOT_USERNAME = environ.get("BOT_USERNAME", "File8372bot") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
@@ -46,7 +46,7 @@ AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
 AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002418235913"))
 
 # File Caption Information
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
